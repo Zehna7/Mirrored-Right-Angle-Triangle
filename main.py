3c69f2e09d2fd0349434c35e269a64ea3db13fbf
Zehna7 <zehna.omar2012@gmail.com>
2024-10-16 23:38:01 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+r = int(input("Enter the Total Number of Rows: "))
+print("Mirrored Right Angle Triangle Star Pattern")
+for i in range(1,r+1):
+    for j in range(1,r+1):
+        if(j <= r - i):
+            print(" ", end = " ")
+        else:
+            print("*", end = " ")
+    print()
